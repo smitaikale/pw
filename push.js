22bec34159f9d52aa0f3481eb25f49ec31b4a8e5
Smita Yashavant Ikale <P7111660@ntil.com>
2020-07-28 17:33:38 +0530

    fix

--- push.js
+++ push.js
@@ -1,4 +1,5 @@
 var push= require('web-push')
+
 let vapidKeys={
     publicKey: 'BBxYcX5EVrEypniCr6VBTC63WQ385spbUUQbygFTUyrF8QEyACdDBb11Wzwv7H1j799F53YIkD1xhcn1NLV6QJc',
     privateKey: 'saADRtj2bQuRjc5wdl6J_3HWDpld_IFqWq7l4b8aScU'
